49831c70b51077e5599e0cb4d323b11ddc17a3a7
nightelf1980 <rodrigo.pereiraarchiles@gmail.com>
2022-08-23 23:55:00 -0400

    primer app react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav class="navbar navbar-expand-lg navbar-light bg-light col-md-11 ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img class="animate__animated animate__fadeIn" src="/img/artemis-logo-color.png" alt="logo" width="100px"></img>
         </a>
-      </header>
-    </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Cactus</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Suculentas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">Productos a la venta</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
