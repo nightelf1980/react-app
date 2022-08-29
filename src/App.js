53bf99fd3fb1aca88eb870e351d2e06196991899
nightelf1980 <rodrigo.pereiraarchiles@gmail.com>
2022-08-29 15:31:39 -0400

    agrega array productos

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ function App() {
     {id:3, title:"Aloe Vera", precio:"2500", descripcion:"", imagen:"aloevera.jpg", categoria:1},
     {id:4, title:"Anthurium Andreanum", precio:"7500", descripcion:"", imagen:"anthuriumandreanum.jpg", categoria:1},
     {id:5, title:"Anthurium Coriaceum", precio:"4800", descripcion:"", imagen:"anthuriumcoriaceum.jpg", categoria:1},
-    {id:6, title:"Aralia Japonica", precio:"4500", descripcion:"", imagen:"araliajaponica.jpg", categoria:1}]
+    {id:6, title:"Aralia Japonica", precio:"4500", descripcion:"", imagen:"araliajaponica.jpg", categoria:1},
+    {id:7,title:"Aspidistra Variegada",precio:"6500",descripcion:"",imagen:"aspidistravariegada.jpg",categoria:1},
+    {id:8,title:"Bacopa",precio:"5000",descripcion:"",imagen:"bacopa.jpg",categoria:1},
+    {id:9,title:"Begonia",precio:"5500",descripcion:"",imagen:"begonia.jpg",categoria:1},
+    {id:10, title:"Begonia Dragon Wing", precio:"6500", descripcion:"", imagen:"begoniadragonwing.jpg", categoria:1},
+    {id:11, title:"Cactus Grusonii", precio:"120000", descripcion:"", imagen:"cactusgrusonii.jpg", categoria:1},
+    {id:12, title:"Cactus injertado", precio:"12000", descripcion:"", imagen:"cactusinjertado.jpg", categoria:1}]
   
   const items = productos.map(items => (
     <ItemListsContainer valor={items.title}/>
