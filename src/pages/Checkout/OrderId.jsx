b4989dd5f266a434d50bac555c4b002cb9e680e4
nightelf1980 <rodrigo.pereiraarchiles@gmail.com>
2022-10-18 21:59:21 -0400

    firebase orden 6

--- src/pages/Checkout/OrderId.jsx
+++ src/pages/Checkout/OrderId.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { useState } from "react";
-import { orderId } from "./FormCheckout"
 
 const OrderId = () => {
-    const [orderId, setOrderId] = useState('')
         return (
             <div className="text-center">
                 <h1>Gracias por su compra</h1>
-                <h3>El número de seguimiento es el: {orderId}</h3>
             </div>
         )
 }
